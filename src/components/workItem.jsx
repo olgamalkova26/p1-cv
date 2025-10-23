03940a2f50420ad1cd5833a51c3b1c03128bb9db
Šimon Janča <jancikuvmail@gmail.com>
2025-10-23 19:10:51 +0200

    Domácí úkol hotový

---
+++ src/components/workItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const WorkItem = ({ icon, title, period }) => {
+  return (
+    <li className="work-item">
+      <img src={icon} alt="ikona práce" className="work-item-icon" />
+      <div className="work-item-content">
+        <span className="work-item-title">{title}</span>
+        <span className="work-item-period">{period}</span>
+      </div>
+    </li>
+  );
+};
+
+export default WorkItem;
