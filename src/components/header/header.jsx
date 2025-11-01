7b1dfd2cae2c37504615aedb05cd9341aa220cd1
Šimon Janča <jancikuvmail@gmail.com>
2025-11-01 10:51:31 +0100

    Add withContext provider component, refactor App component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './header.css';
 
 const Header = ({ name, email, phone, website, photoSrc }) => {
@@ -10,7 +11,9 @@ const Header = ({ name, email, phone, website, photoSrc }) => {
                 📧 {email} | ☎️ {phone} | 🌐 {website}
             </p>
         </div>
-        <img className="cv-photo" src={photoSrc} alt="Profilová fotka" />
+        <NavLink to="/profile" >
+            <img className="cv-photo" src={photoSrc} alt="Profilová fotka" />
+        </NavLink>
     </header>
 }
 
