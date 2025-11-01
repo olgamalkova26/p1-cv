f13d3be1827a1b06b984a5341214f06dbbd2bcab
Šimon Janča <jancikuvmail@gmail.com>
2025-11-01 09:33:23 +0100

    Fix naming for theme context

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import ThemeContext from './utils/context/provider.jsx'
+import ThemeContextProvider from './utils/context/themeProvider.comp.jsx'
 
 import ErrorBoundary from './components/errorBoundary.jsx'
 import CVPage from './App.jsx'
@@ -14,15 +14,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const App = () => {
   const queryClient = new QueryClient();
 
-  return <ThemeContext>
+  return <ThemeContextProvider>
     <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path="/" element={<CVPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={<ProfilePage personalInfo={{
+
+        }} />} />
         <Route path="*" Component={() => <div>404 Not Found</div>} />
       </Routes>
     </QueryClientProvider>
-  </ThemeContext>
+  </ThemeContextProvider>
 }
 
 /**
